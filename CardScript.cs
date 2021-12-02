b42ea58522684784320faef4a155feca41ba998b
David Gil <dagipe01@floridauniversitaria.es>
2021-12-02 18:38:49 +0100

    Commit inicial

---
+++ CardScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    public Sprite spriteBack;
+   
+    GameObject carta;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GetComponent<SpriteRenderer>().sprite = spriteBack;
+        transform.position = new Vector3(3, 3, 0);
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
