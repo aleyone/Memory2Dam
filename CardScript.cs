bb4be62572bb37557ca7e21abd3fefc5acd7a433
David Gil <dagipe01@floridauniversitaria.es>
2021-12-03 23:04:40 +0100

    Commit Ejercicio 2

--- CardScript.cs
+++ CardScript.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class CardScript : MonoBehaviour
 {
     
-   
+
     // Start is called before the first frame update
     void Start()
     {
 
-        
-       
-        
+
+
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("Click");
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 }
